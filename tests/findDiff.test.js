a1bfb1454f1b90b3487271e2bb0c0a2516a4d2db
Andre Soesilo <andre@axiomzen.co>
2016-01-21 16:38:54 -0800

    Implement findDiff

--- tests/findDiff.test.js
+++ tests/findDiff.test.js
@@ -1,10 +1,19 @@
 var expect = require('chai').expect;
 var findDiff = require('../util/findDiff');
 
-describe("Find Diff | ", function(){
+describe.only("Find Diff | ", function(){
   it("base cases", function(){
     expect(findDiff(null, null)).to.equal(null);
-    expect(findDiff([], [])).to.equal(null);
+  });
+
+  it("returns null for empty preferences", function() {
+    var candy = {
+      id: 0
+    };
+    expect(findDiff(candy, {})).to.equal(null);
+
+    candy.prefs = [];
+    expect(findDiff(candy, {})).to.equal(null);
   });
 
   it("candy diffs", function(){
@@ -13,9 +22,10 @@ describe("Find Diff | ", function(){
       prefs: [5, 4, 1]
     };
 
-    expect(findDiff(candy, [])).to.equal(1);
-    expect(findDiff(candy, [0])).to.equal(3);
-    expect(findDiff(candy, [1])).to.equal(4);
-    expect(findDiff(candy, [2])).to.equal(1);
+    expect(findDiff(candy, {})).to.equal(1);
+    expect(findDiff(candy, null)).to.equal(1);
+    expect(findDiff(candy, {0: true})).to.equal(3);
+    expect(findDiff(candy, {1: true})).to.equal(4);
+    expect(findDiff(candy, {2: true})).to.equal(1);
   });
 });
--- util/findDiff.js
+++ util/findDiff.js
@@ -1,3 +1,30 @@
-module.exports = function(candyObj, tabooList) {
-  return null ;
+var INF_NEG = -10000000
+
+module.exports = function(candyObj, taboo) {
+  if(!candyObj || !candyObj.prefs || candyObj.prefs.length === 0) {
+    return null;
+  }
+
+  taboo = taboo || {};
+  var prefs = candyObj.prefs;
+  var highest = null;
+  var secondHighest = null;
+  for(var i = 0; i < prefs.length; i++) {
+    if(!taboo[i]) {
+      // Current index is not in taboo list so it can be used
+      var current = prefs[i];
+      if(current > (highest || INF_NEG)) {
+        // Found a new highest value
+        secondHighest = highest;
+        highest = current;
+      }
+      else if(current > (secondHighest || INF_NEG)) {
+        // Found a new second highest value
+        secondHighest = current;
+      }
+    }
+  }
+  highest = highest || 0;
+  secondHighest = secondHighest || 0;
+  return Math.abs(highest - secondHighest);
 };
