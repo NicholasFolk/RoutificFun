e349e3251d16363d5a40351384ad2aa2c7ebc5a1
Andre Soesilo <andre@axiomzen.co>
2016-01-21 16:11:51 -0800

    Add integration test

--- tests/full.test.js
+++ tests/full.test.js
@@ -1,6 +1,31 @@
+var candyOrganizer = require('candyOrganizer');
 var expect = require('chai').expect;
+var Random = require('./utils/random');
+
 describe("Full test | ", function(){
-  it("simple test", function(){
-    expect(1).to.be.equal(1);
-  })
+  it.skip("should return null for null input", function(){
+    var res = candyOrganizer(null);
+    expect(res).to.eq(null);
+  });
+
+  it.skip("should return null for an empty array ", function(){
+    var res = candyOrganizer([]);
+    expect(res).to.eq(null);
+  });
+
+  it("should return index zero for 1x1 matrix", function(){
+    randomVal = Random.integer(100);
+    var res = candyOrganizer([randomVal]);
+    expect(res).to.eq([0]);
+  });
+
+  it("should return correct indices order for 3x3 matrix", function(){
+    var data = [
+      [3,2,0],
+      [0,3,10],
+      [1,5,2]
+    ]
+    var res = candyOrganizer(data);
+    expect(res).to.eq([0,2,1])
+  });
 });
